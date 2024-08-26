86c8c7e23b10c69861513d38b48dcb10d1f87fd6
Rüdiger Weritz <16781187+rweritz@users.noreply.github.com>
2024-08-26 14:58:16 +0200

    -Updated to dotnet 8 -New Dockerfiles created for the different image types including 3 different OS options -docker-compose added for all the 12 different image builds -http test file added to test running containers

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateSlimBuilder(args);
+
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
+});
 
 // Add services to the container.
-
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -12,14 +18,42 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
+    app.UseSwagger(c => c.RouteTemplate = "swagger/{documentName}/swagger.{json|yaml}");
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
 
-app.MapControllers();
+app.MapGet("/weatherforecast", () =>  
+    {  
+        var forecast =  Enumerable.Range(1, 5).Select(index =>  
+                new WeatherForecast  
+                (  
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),  
+                    Random.Shared.Next(-20, 55),  
+                    summaries[Random.Shared.Next(summaries.Length)]  
+                ))  
+            .ToArray();
+        return forecast;
+    })  
+    .WithName("GetWeatherForecast")
+    .WithOpenApi();
 
 app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)  
+{  
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);  
+}
+
+[JsonSourceGenerationOptions(WriteIndented = true)]
+[JsonSerializable(typeof(WeatherForecast[]))]
+internal partial class AppJsonSerializerContext : JsonSerializerContext
+{
+
+}
